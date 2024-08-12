eb93620172fcf8197cdc3c4fd8dc6e4b752635f5
Daniel Chin <DanChin327@gmail.com>
2024-08-12 15:31:30 +0900

    added the pull of API

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// User types in a city
+// The App identifies the city
+ // If city exits, will find JSON.
+ // If not, make a city not found message
+
+// needs to iterate and provid the five days.
+
+// "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
+// ex. api.openweathermap.org/data/2.5/forecast?q=tokyo&appid=89638d41ac4d76e85c0eca18872b79bb
 
 const apiKey = "89638d41ac4d76e85c0eca18872b79bb" //API is free so keep it here.
-const apiURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
+const apiURL = "api.openweathermap.org/data/2.5/forecast?q="
+
 
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -14,6 +24,6 @@ search.addEventListener("submit", printCity)
 
 function printCity(e) {
 e.preventDefault()
-const searchInput = document.querySelector('#city-name').value
-console.log(`City you put: ${searchInput}`)
+const cityInput = document.querySelector('#city-name').value
+const apiPull = `${apiURL}${cityInput}&appid=${apiKey}`
 }
