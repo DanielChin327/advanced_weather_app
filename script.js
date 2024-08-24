efcb00efb7564ff8deb124b249ff1c40e44f50e7
Daniel Chin <DanChin327@gmail.com>
2024-08-24 16:54:48 +0900

    fixing styling

--- script.js
+++ script.js
@@ -11,22 +11,50 @@
 const apiKey = "89638d41ac4d76e85c0eca18872b79bb" //API is free so keep it here.
 const apiURL = "api.openweathermap.org/data/2.5/forecast?q="
 
-document.addEventListener('DOMContentLoaded', function() {
 
-const search = document.querySelector('#search')
-search.addEventListener("submit", printCity)
+async function checkWeather(city){
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if(response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  }
+  else{
+  var data = await response.json();
+  console.log(data)
+  var f = Math.round(((data.main.temp) * 9/5) + 32)
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c" +` / ${f}°f`;
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  if(data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  }
+  else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  }
+  else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  }
+  else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  }
+  else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
+
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".error").style.display = "none";
+  }
+
+}
+searchBtn.addEventListener("click", ()=> {
+checkWeather(searchBox.value);
 })
 
 
-function printCity(e) {
-e.preventDefault()
-const cityInput = document.querySelector('#city-name').value
-const apiPull = `https://${apiURL}${cityInput}&appid=${apiKey}`
-
-fetch(apiPull)
-  .then(response=> response.json())
-  .then(data => console.log(data))
-
-  .catch(error => console.error('Error fetching data:', error));
-
+searchBox.addEventListener("keypress", (event) => {
+if (event.key === "Enter") {
+  checkWeather(searchBox.value);
 }
+});
